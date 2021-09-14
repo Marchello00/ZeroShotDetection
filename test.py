ac82e9b19cb6810ee62080d4f21c88dcb8374b37
Mark Nagovitsin <nagovitsin.ms@phystech.edu>
2021-09-14 15:43:51 +0300

    Test

--- test.py
+++ test.py
@@ -15,57 +15,13 @@ def round_struct(struct, digits):
 def test_respond():
     url = "http://0.0.0.0:8088/respond"
 
-    text = ["Show me the dog, please!",
-            "Найди собаку.",
-            "Где здесь кот?"]
-    image = ["dog.jpg", "dog.jpg", "dog.jpg"]
-
     request_data = {
-        "text": text,
-        "image": image
+        "dog.jpg": ['dog', 'кот'],
     }
 
     result = requests.post(url, json=request_data).json()
 
-    gold_result = {'detected_objects': [
-        [
-            {
-                'l': 36.53,
-                'u': 0.0,
-                'r': 1025.93,
-                'd': 1038.18,
-                'label_en': 'Samoyed',
-                'label_ru': 'Самоед',
-            },
-            {
-                'l': 382.75,
-                'u': 447.38,
-                'r': 1546.0,
-                'd': 1130.21,
-                'label_en': 'Great Pyrenees',
-                'label_ru': 'Великие Пиренеи',
-            }
-        ],
-        [
-            {
-                'l': 36.53,
-                'u': 0.0,
-                'r': 1025.93,
-                'd': 1038.18,
-                'label_en': 'Samoyed',
-                'label_ru': 'Самоед',
-            },
-            {
-                'l': 382.75,
-                'u': 447.38,
-                'r': 1546.0,
-                'd': 1130.21,
-                'label_en': 'Great Pyrenees',
-                'label_ru': 'Великие Пиренеи',
-            }
-        ],
-        []
-    ]}
+    gold_result = {'dog.jpg': [['dog', 36.53, 0.0, 989.4, 1038.18]]}
 
     digits = 2
     result = round_struct(result, digits)
